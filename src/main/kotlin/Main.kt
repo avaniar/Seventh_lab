50591404a14b11a1641c13368e185b72ae16abab
MaksimVasin <maksim.vasin.2001@mail.ru>
2021-12-15 12:50:42 +0300

    add main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,2 +1,18 @@
 fun main() {
+    val pathWrite = "C:\\Seventh_lab\\src\\main\\kotlin\\data.json"
+    val serialize = SerializeShapes
+    val shapeFactory = ShapeFactoryImpl()
+    println("Data obtained")
+    println(WorkWithFiles.read(pathWrite))
+    val shapeList = serialize.decode(WorkWithFiles.read(pathWrite))
+    shapeList.addAll(
+        listOf(
+            shapeFactory.createRandomShape(),
+            shapeFactory.createRandomShape(),
+            shapeFactory.createRandomShape()
+        )
+    )
+    println("Modified data")
+    println(serialize.encode(shapeList))
+    WorkWithFiles.write(pathWrite, serialize.encode(shapeList))
 }
